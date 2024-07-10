efce3a2284acd7b0d52e54b7d7fa51935f9242ac
Aryan Sood <aryansood@Aryans-MacBook-Air.local>
2024-07-10 21:33:17 +0530

    added sentiment

---
+++ sentiment_analysis.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from functools import reduce
+import pandas as pd, numpy as np , networkx as nx 
+from transformers import pipeline, AutoTokenizer
+
+file_path = '/Users/aryansood/Github/Music-Recommender-System/song_data/full_df.pkl'
+df = pd.read_pickle(file_path)
+
+df['sentiment'] = '[]'
+required_cols = ["track_name" , 'track_id', 'explicit', 'artist_names',
+       'artist_ids', 'acousticness', 'danceability', 'energy',
+       'instrumentalness', 'liveness', 'loudness',
+       'speechiness', 'tempo', 'popular_artist',
+       'popular_artist_id', 'combined_genres', 'lyrics']
+df = df.loc[: ,required_cols]
+
+
+# Initialize Transformers pipeline and tokenizer
+pipe = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", device=0)
+tokenizer = AutoTokenizer.from_pretrained("j-hartmann/emotion-english-distilroberta-base")
+
+def sentiments (lrcs) : 
+    tokens = tokenizer(lrcs, truncation=True, max_length=512, return_tensors="pt")
+    truncated_lrcs = tokenizer.decode(tokens.input_ids[0], skip_special_tokens=True)
+    output = pipe(truncated_lrcs)
+    scores = {i["label"]: i["score"] for i in output if i["score"] > 0.1}
+    lst = ["anger", "disgust", "fear", "joy", "neutral", "sadness", "surprise"]
+    sentiments = [scores.get(key, 0) for key in lst]
+    # sentiments = []
+    return sentiments
+
+for index , row in df.iterrows() : 
+    lrcs = str(row["lyrics"])
+    if lrcs == "nan" : 
+        df.loc[index , "sentiments" ] = str([0,0,0,0,0,0,0])
+        continue
+    df.loc[index , "sentiments" ] = str(sentiments(lrcs))
+    print(f"current song : {index}\r")
+    if index % 10 == 0: 
+        df.to_pickle("sentiment_data.pkl")
+        print( " "*50 , f"Concatenated and saved {index} songs\r")
+
